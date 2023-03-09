6f388791e390fdb4913bbc888880057f176d235d
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-08 22:25:18 -0300

    Add project files.

---
+++ MatrizMedia/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[,] mat = new double[5, 3];
+        LerMatriz(mat);
+        MostrarMatriz(mat);
+
+        void LerMatriz(double[,] x)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        Console.WriteLine("Digite o ["+ i + "," + j + "] valor da matriz: ");
+                        x[i, j] = double.Parse(Console.ReadLine());
+                    }
+            }
+            for (int i = 0; i < 5;i++)
+            {
+                x[i,2] = x[i,0] + x[i,1];
+            }
+        }
+        void MostrarMatriz(double[,] y)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(y[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
